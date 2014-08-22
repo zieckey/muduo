50f8c36b22ed5475ed8afdb3dd0c959200c4e024
weizili.build17 <zieckey@gmail.com>
2014-08-22 13:59:28 +0800

    Add UdpClieht, compile pass

---
+++ muduo/net/udp/UdpClient.cc
@@ -0,0 +1,277 @@
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+
+// Author: zieckey (zieckey at gmail dot com)
+//
+
+#include <muduo/net/udp/UdpClient.h>
+
+#include <muduo/base/Logging.h>
+#include <muduo/net/Connector.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/SocketsOps.h>
+
+#include <boost/bind.hpp>
+
+#include <stdio.h>  // snprintf
+#include <errno.h>  // snprintf
+
+using namespace muduo;
+using namespace muduo::net;
+
+// UdpClient::UdpClient(EventLoop* loop)
+//   : loop_(loop)
+// {
+// }
+
+// UdpClient::UdpClient(EventLoop* loop, const string& host, uint16_t port)
+//   : loop_(CHECK_NOTNULL(loop)),
+//     serverAddr_(host, port)
+// {
+// }
+
+namespace muduo
+{
+namespace net
+{
+namespace detail
+{
+typedef struct sockaddr SA;
+const SA* sockaddr_cast(const struct sockaddr_in* addr)
+{
+  return static_cast<const SA*>(implicit_cast<const void*>(addr));
+}
+}
+}
+}
+
+UdpClient::UdpClient(EventLoop* loop,
+                     const InetAddress& serverAddr,
+                     const string& name)
+  : loop_(CHECK_NOTNULL(loop)),
+    name_(name),
+    serverAddr_(serverAddr),
+    //messageCallback_(defaultMessageCallback), //TODO
+    retry_(false),
+    connect_(false),
+    nextConnId_(1)
+{
+  LOG_INFO << "UdpClient::UdpClient[" << name_
+           << "] - remoteAddr " << serverAddr_.toIpPort();
+}
+
+UdpClient::~UdpClient()
+{
+  LOG_INFO << "UdpClient::~UdpClient[" << name_
+           << "] - channel_ " << get_pointer(channel_);
+  loop_->assertInLoopThread();
+  close();
+}
+
+bool UdpClient::connect()
+{
+  LOG_INFO << "UdpClient::connect[" << name_ << "] - connecting to "
+           << serverAddr_.toIpPort();
+  assert(!connect_);
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  assert(sockfd > 0);
+  if (sockfd < 0) {
+    return false;
+  }
+
+  const struct sockaddr_in& remoteAddr = serverAddr_.getSockAddrInet();
+  socklen_t addrLen = sizeof(remoteAddr);
+  int ret = ::connect(sockfd, detail::sockaddr_cast(&remoteAddr), addrLen);
+
+  if (ret != 0)
+  {
+    int  savedErrno = errno;
+    close();
+    const struct sockaddr_in *paddr = &remoteAddr;
+    LOG_ERROR << "Failed to connect to remote IP="
+        << inet_ntoa(paddr->sin_addr)
+        << ", port=" << ntohs(paddr->sin_port)
+        << ", errno=" << savedErrno;
+    ::close(sockfd);
+    return false;
+  }
+
+  channel_.reset(new Channel(getLoop(), sockfd));
+  channel_->setReadCallback(
+      boost::bind(&UdpClient::handleRead, this, _1));
+//  channel_->setWriteCallback(
+//      boost::bind(&UdpClient::handleWrite, this));
+//  channel_->setCloseCallback(
+//      boost::bind(&UdpClient::handleClose, this));
+//  channel_->setErrorCallback(
+//      boost::bind(&UdpClient::handleError, this));
+
+  connect_ = true;
+  return true;
+}
+
+void UdpClient::close()
+{
+  if (loop_->isInLoopThread())
+  {
+    closeInLoop();
+  }
+  else
+  {
+    loop_->runInLoop(
+        boost::bind(&UdpClient::closeInLoop, this));  
+  }
+}
+
+void UdpClient::closeInLoop()
+{
+  loop_->assertInLoopThread();
+  connect_ = false;
+  if (channel_.get()) {
+    if (channel_->fd() > 0) {
+      ::close(channel_->fd());
+    }
+    channel_->disableAll();
+    channel_->remove();
+    channel_.reset();
+  }
+}
+
+void UdpClient::send(const void* data, size_t len)
+{
+  assert(connect_);
+  if (connect_)
+  {
+    if (loop_->isInLoopThread())
+    {
+      sendInLoop(data, len);
+    }
+    else
+    {
+      string message(static_cast<const char*>(data), len);
+      loop_->runInLoop(
+          boost::bind(&UdpClient::sendInLoop,
+                      this,     // FIXME
+                      message));
+    }
+  }
+}
+
+void UdpClient::send(const StringPiece& message)
+{
+  assert(connect_);
+  if (connect_)
+  {
+    if (loop_->isInLoopThread())
+    {
+      sendInLoop(message);
+    }
+    else
+    {
+      loop_->runInLoop(
+          boost::bind(&UdpClient::sendInLoop,
+                      this,
+                      message.as_string()));
+    }
+  }
+}
+
+// FIXME efficiency!!!
+void UdpClient::send(Buffer* buf)
+{
+  assert(connect_);
+  if (connect_)
+  {
+    if (loop_->isInLoopThread())
+    {
+      sendInLoop(buf->peek(), buf->readableBytes());
+      buf->retrieveAll();
+    }
+    else
+    {
+      loop_->runInLoop(
+          boost::bind(&UdpClient::sendInLoop,
+                      this,
+                      buf->retrieveAllAsString()));
+    }
+  }
+}
+
+void UdpClient::sendInLoop(const StringPiece& message)
+{
+  sendInLoop(message.data(), message.size());
+}
+
+void UdpClient::sendInLoop(const void* data, size_t len)
+{
+  loop_->assertInLoopThread();
+  ssize_t nwrote = 0;
+  bool error = false;
+  if (!connect_)
+  {
+    LOG_WARN << "disconnected, give up writing";
+    return;
+  }
+
+  //send udp data directly
+  const struct sockaddr_in& remoteAddr = serverAddr_.getSockAddrInet();
+  socklen_t addrLen = sizeof(remoteAddr);
+  nwrote = ::sendto(channel_->fd(), data, len, 0,
+              detail::sockaddr_cast(&remoteAddr), addrLen);
+  if (nwrote >= 0)
+  {
+  }
+  else // nwrote < 0
+  {
+    nwrote = 0;
+    if (errno != EWOULDBLOCK)
+    {
+      LOG_SYSERR << "UdpClient::sendInLoop";
+      if (errno == EPIPE) // FIXME: any others?
+      {
+        error = true;
+      }
+    }
+
+    //TODO FIXME fix this routine problem : when sendto return -1
+  }
+}
+
+void UdpClient::handleRead(Timestamp receiveTime)
+{
+  loop_->assertInLoopThread();
+  //int savedErrno = 0;
+  size_t initialSize = 1472; // The UDP max payload size
+  BufferPtr inputBuffer(new Buffer(initialSize));
+  struct sockaddr remoteAddr;
+  socklen_t addrLen = sizeof(remoteAddr);
+  ssize_t readn = ::recvfrom(channel_->fd(), inputBuffer->beginWrite(), 
+      inputBuffer->writableBytes(),
+      0, &remoteAddr, &addrLen);
+  int savedErrno = sockets::getSocketError(channel_->fd());
+  if (readn == 0)
+  {
+    //TODO ERROR process
+    LOG_ERROR << "errno=" << savedErrno << " " << strerror(savedErrno) << " recvfrom return " << readn;
+    //handleClose();
+  }
+  else if (readn > 0)
+  {
+    inputBuffer->hasWritten(readn);
+    if (messageCallback_) {
+      messageCallback_(shared_from_this(), inputBuffer, receiveTime);
+    }
+  }
+  else
+  {
+      //TODO
+    LOG_ERROR << "errno=" << savedErrno << " " << strerror(savedErrno) << " recvfrom return " << readn;
+    //handleError();
+  } 
+}
+
+
+
---
+++ muduo/net/udp/UdpClient.h
@@ -0,0 +1,106 @@
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+
+// Author: zieckey (zieckey at gmail dot com)
+//
+// This is a public header file, it must only include public header files.
+
+#ifndef MUDUO_NET_UDPCLIENT_H
+#define MUDUO_NET_UDPCLIENT_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/any.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <muduo/base/Mutex.h>
+#include <muduo/base/Timestamp.h>
+#include <muduo/net/Channel.h>
+#include <muduo/net/InetAddress.h>
+#include <muduo/net/Buffer.h>
+#include <muduo/net/Callbacks.h>
+
+namespace muduo
+{
+namespace net
+{
+
+class UdpClient;
+typedef boost::shared_ptr<UdpClient> UdpClientPtr;
+typedef boost::shared_ptr<Buffer> BufferPtr;
+
+class UdpClient : boost::noncopyable, 
+                  public boost::enable_shared_from_this<UdpClient>
+{
+ public:
+  typedef boost::function<void (const UdpClientPtr&,
+                              BufferPtr&,
+                              Timestamp)> UdpMessageCallback;
+ public:
+  // UdpClient(EventLoop* loop, const string& host, uint16_t port);
+  UdpClient(EventLoop* loop,
+            const InetAddress& serverAddr,
+            const string& name);
+  ~UdpClient();  // force out-line dtor, for scoped_ptr members.
+
+  /// connect the remote server
+  bool connect();
+
+  /// close the socket
+  void close();
+
+  EventLoop* getLoop() const { return loop_; }
+  bool retry() const;
+  void enableRetry() { retry_ = true; }
+
+  // void send(string&& message); // C++11
+  void send(const void* message, size_t len);
+  void send(const StringPiece& message);
+  // void send(Buffer&& message); // C++11
+  void send(Buffer* message);  // this one will swap data
+
+
+  void setContext(const boost::any& context)
+  { context_ = context; }
+
+  const boost::any& getContext() const
+  { return context_; }
+
+  boost::any* getMutableContext()
+  { return &context_; }
+
+  /// Set message callback.
+  /// Not thread safe.
+  void setMessageCallback(const UdpMessageCallback& cb)
+  { messageCallback_ = cb; }
+
+  /// Set write complete callback.
+  /// Not thread safe.
+  //void setWriteCompleteCallback(const WriteCompleteCallback& cb)
+  //{ writeCompleteCallback_ = cb; }
+ private:
+  void closeInLoop();
+  //void sendInLoop(string&& message);
+  void sendInLoop(const StringPiece& message);
+  void sendInLoop(const void* message, size_t len);
+  void handleRead(Timestamp receiveTime);
+ private:
+  EventLoop* loop_;
+  const string name_;
+  InetAddress serverAddr_;
+  UdpMessageCallback messageCallback_;
+  //WriteCompleteCallback writeCompleteCallback_;
+  boost::scoped_ptr<Channel> channel_;
+  bool retry_;   // atomic 
+  bool connect_; // atomic 
+  int nextConnId_;
+  boost::any context_;
+};
+
+}
+}
+
+#endif  // MUDUO_NET_TCPCLIENT_H
