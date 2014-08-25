be009f2551826c9f5174233b679d8ace81f20488
weizili.build17 <zieckey@gmail.com>
2014-08-25 20:51:54 +0800

    fix rpm.sh error

--- rpm.sh
+++ rpm.sh
@@ -1,11 +1,11 @@
 git co build
+cd ..
 TARGET=home/s/safe
 rm -rf $TARGET
 mkdir $TARGET/lib -p
 mkdir $TARGET/include/muduo/base -p
 mkdir $TARGET/include/muduo/net -p
 
-cd ..
 cp -rf build/release/lib/* $TARGET/lib
 cp -rf muduo/muduo $TARGET/include
 find $TARGET -name *.cc | xargs rm -rf
