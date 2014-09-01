8520c62171bba24bbebba72c10033bba194a06c0
weizili.build17 <zieckey@gmail.com>
2014-09-01 21:03:26 +0800

    Add debug lib

--- rpm/tar.sh
+++ rpm/tar.sh
@@ -4,10 +4,12 @@ cd ../..
 TARGET=home/s/safe
 rm -rf $TARGET
 mkdir $TARGET/lib -p
+mkdir $TARGET/lib/debug -p
 mkdir $TARGET/include/muduo/base -p
 mkdir $TARGET/include/muduo/net -p
 
 cp -rf build/release/lib/* $TARGET/lib
+cp -rf build/debug/lib/* $TARGET/lib/debug
 cp -rf muduo/muduo $TARGET/include
 find $TARGET -name *.cc | xargs rm -rf
 find $TARGET -name *.txt | xargs rm -rf
